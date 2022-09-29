9e247caa7269b49c9d1c7885aee3e6df24a6f2dd
Vince Maina <vchapandrews@gmail.com>
2022-09-30 00:46:53 +0100

    Adding user model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 
-class Test(db.Model):
-    number = db.Column(db.Integer, primary_key=True)
+class User(UserMixin, db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return self.name
