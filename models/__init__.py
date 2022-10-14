2c4f07de1324f8403a5a7a81577173d5c190d62d
Vince Maina <vchapandrews@gmail.com>
2022-10-14 01:23:33 +0100

    Moved models dir. Expanded user model

---
+++ models/__init__.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    # Account details
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email_address = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    profile_image = db.Column(db.String(150))
+    role = db.Column(db.String(100), nullable=False, default='super_admin')
+
+    # Account setup
+    email_verified = db.Column(db.Boolean, nullable=False, default=False)
+    twitter_connected = db.Column(db.Boolean, nullable=False, default=False)
+    business_information_provided = db.Column(db.Boolean, nullable=False, default=False)
+    onboarded = db.Column(db.Boolean, nullable=False, default=False)
+
+    # Business details
+    company_name = db.Column(db.String(100))
+
+    def __repr__(self):
+        return self.name
+
+
+class Campaigns(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    is_live = db.Column(db.Boolean, nullable=False, default=False)
+    filters = db.Column(db.Text)  # A list of client-side filters
