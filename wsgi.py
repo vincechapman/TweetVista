c09b38448f3cd761924e890afbe2011d2abb3adb
Vince Maina <vchapandrews@gmail.com>
2022-10-14 01:30:28 +0100

    Clear db every time server is started + set admin user properly

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,31 @@
 import os
 from dotenv import load_dotenv
-from app import create_app, models
+from app import create_app
+from werkzeug.security import generate_password_hash
+import models
 
 load_dotenv()
 
 tweet_vista = create_app()
+
+# Removing old database instance
+if os.path.exists("app/db.sqlite"):
+    os.remove("app/db.sqlite")
+    print("Old sqlite database has been deleted successfully")
+
+# Creating database models
 models.db.create_all(app=tweet_vista)
+print("New database created!")
 
 # Creating Super Admin if it doesn't already exist
 with tweet_vista.app_context():
+
     if not models.User.query.get(1):
         new_user = models.User(
-            email=os.environ['ADMIN_EMAIL'],
-            password=os.environ['ADMIN_PASSWORD'],
-            name=os.environ['ADMIN_NAME'],
+            email_address=os.environ['ADMIN_EMAIL'],
+            password=generate_password_hash(os.environ['ADMIN_PASSWORD']),
+            first_name=os.environ['ADMIN_FIRST_NAME'],
+            last_name=os.environ['ADMIN_LAST_NAME'],
             role='super_admin'
         )
         models.db.session.add(new_user)
