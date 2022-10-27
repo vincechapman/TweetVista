11342b0d765929d9d871bf77446b8274677979de
Vince Maina <vchapandrews@gmail.com>
2022-10-27 18:20:07 +0100

    Copied in functions for authenticating user so that I could get rid of old repos

--- TLInterface/__init__.py
+++ TLInterface/__init__.py
@@ -3,15 +3,38 @@ This module serves as an intermediary between my frontend application and Dave's
 """
 
 import logging
-from TLApi.AccessTLIServer.access_tli import WebConnection, web_conn, authenticate_user
+# from TLApi.AccessTLIServer.access_tli import WebConnection, web_conn, authenticate_user
+from TLApiWeb.connect import WebConnection
 
 
-def get_web_connection() -> WebConnection or bool:
+def authenticate_user(user, password):
+    wc = WebConnection()
+    if wc.url is None:
+        wc.url = 'https://tlapi.ictbenchmark.org/'
+    wc.userid = user
+    wc.password = password
+    wc.set_headers()
+    ret_dict = wc.authenticate_user()
+    logging.info(f'{ret_dict}')
+    ret_dict = wc.is_authenticated()
 
-    # Line here that checks if there is already a wc saved in session object, if so return that. Otherewise return below code.
+
+def web_conn(usr=None, pwd=None, url=None, sessionid=None):
+    wc = WebConnection(
+        userid=usr,
+        password=pwd,
+        url=url,
+        sessionid=sessionid
+    )
+    return wc
+
+
+def get_web_connection(user: str = 'd.hagan', password: str = 'changeme') -> WebConnection or bool:
+
+    # Line here that checks if there is already a wc saved in session object, if so return that, else return below code.
 
     try:
-        authenticate_user()
+        authenticate_user(user, password)
     except Exception as e:
         logging.error(f'Failed to authenticate user, due to following error:\n\n{e}')
         return False
@@ -22,5 +45,5 @@ def get_web_connection() -> WebConnection or bool:
         logging.error(f'Failed to create web connection, due to following error:\n\n{e}')
         return False
     else:
-        # Add an option here for saving wc to a session object if doesn't already exist
+        # Add an option here for saving wc to a session object if it doesn't already exist
         return wc
