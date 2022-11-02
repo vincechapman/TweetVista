bebbc121a9476a0b1aa75200e21a2f2ae1c41272
Vince Maina <vchapandrews@gmail.com>
2022-11-02 19:53:47 +0000

    Added routes for starting, stopping and deleting campaigns

--- app/blueprints/api/campaigns.py
+++ app/blueprints/api/campaigns.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from flask import Blueprint, request, jsonify
 
@@ -25,3 +26,64 @@ def get_campaign_data():
     print(campaign_data)
 
     return jsonify(campaign_data)
+
+
+@api_campaigns.route('/startStopCampaign', methods=['POST'])
+def start_stop_campaign():
+
+    print('POST api/campaigns/startStopCampaign')
+
+    try:
+        request_body = json.loads(request.data)
+    except json.decoder.JSONDecodeError as e:
+        print('FAILED: request_body = json.loads(request.data)')
+        print(e)
+        return 'Failed to get stream.', 404
+    else:
+        campaign_id = request_body['campaignID']
+        campaign_name = request_body['campaignName']
+        start_or_stop = request_body['start_or_stop']
+
+    from TLInterface import get_web_connection
+    wc = get_web_connection()
+
+    if start_or_stop == 'start':
+        response = wc.start_campaign_feed(
+            campaign_id=campaign_id,
+            campaign_name=campaign_name
+        )
+        logging.info(f'Campaign {campaign_id} ({campaign_name}) started.')
+    else:
+        response = wc.stop_campaign_feed(
+            campaign_id=campaign_id,
+            campaign_name=campaign_name
+        )
+        logging.info(f'Campaign {campaign_id} ({campaign_name}) stopped.')
+    return jsonify(response)
+
+
+@api_campaigns.route('/deleteCampaign', methods=['POST'])
+def delete_campaign():
+
+    print('POST api/campaigns/deleteCampaign')
+
+    try:
+        request_body = json.loads(request.data)
+    except json.decoder.JSONDecodeError as e:
+        print('FAILED: request_body = json.loads(request.data)')
+        print(e)
+        return 'Failed to get stream.', 404
+    else:
+        campaign_id = request_body['campaignID']
+        campaign_name = request_body['campaignName']
+
+    from TLInterface import get_web_connection
+    wc = get_web_connection()
+
+    response = wc.delete_campaign(
+        campaign_id=campaign_id,
+        campaign_name=campaign_name
+    )
+    logging.info(f'Campaign {campaign_id} ({campaign_name}) started.')
+
+    return jsonify(response)
