26b78399747d8579fd8de67105cc7bd387ba8fb6
Vince Maina <vchapandrews@gmail.com>
2022-11-02 19:55:52 +0000

    Added functionality for creating new campaigns

--- app/blueprints/campaigns.py
+++ app/blueprints/campaigns.py
@@ -1,14 +1,119 @@
-from flask import Blueprint, render_template, request, session
+import logging
+
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required
 
 campaigns = Blueprint('campaigns', __name__, url_prefix='/campaigns')
 
 
-@campaigns.route('/')
+@campaigns.route('/', methods=['GET', 'POST'])
 def main():
+
+    if request.method == 'POST':
+
+        if request.form['submit'] == 'create-campaign':
+
+            from TLInterface.get_campaigns import get_web_connection
+            wc = get_web_connection()
+
+            form_data = request.form.to_dict()
+
+            # Creating a new campaign
+
+            if form_data['campaign-type'] != 'affiliate':
+                form_data['affiliate-name'] = None
+                form_data['affiliate-url'] = None
+                form_data['affiliate-short-url'] = None
+
+            response = wc.create_client_campaign(
+                campaign_name=form_data['campaign-name'],
+                campaign_type=form_data['campaign-type'],
+                affiliate_name=form_data['affiliate-name'],
+                affiliate_url=form_data['affiliate-url'],
+                affiliate_short=form_data['affiliate-short-url']
+            )
+
+            print('Create client campaign response: ', response)
+
+            if response['status'] == 200:
+
+                new_campaign_id = response['data']['campaign_id']
+
+                # Adding keywords for new campaign
+
+                keywords = {
+                    'positive': {},
+                    'negative': {}
+                }
+
+                for field in form_data:
+
+                    if field.startswith('positive-keywords-field') or field.startswith('negative-keywords-field'):
+                        data = field.split('|')
+                        row_number = data[1]
+                        data_type = data[2]
+                        value = form_data[field]
+
+                        if field.startswith('positive-keywords-field'):
+                            try:
+                                keywords['positive'][row_number]
+                            except KeyError:
+                                keywords['positive'][row_number] = {}
+                            finally:
+                                keywords['positive'][row_number][data_type] = value
+
+                        elif field.startswith('negative-keywords-field'):
+                            try:
+                                keywords['negative'][row_number]
+                            except KeyError:
+                                keywords['negative'][row_number] = {}
+                            finally:
+                                keywords['negative'][row_number][data_type] = value
+
+                from pprint import pprint
+                pprint(keywords, indent=4)
+
+                for kw_set in keywords:
+                    for kw in keywords[kw_set]:
+                        keyword = keywords[kw_set][kw]['keyword']
+                        kw_type = keywords[kw_set][kw]['type']
+                        if kw_type == 'Exact Order':
+                            order = 'fixed'
+                        elif kw_type == 'Any Order':
+                            order = 'any'
+                        else:
+                            logging.warning('There is no order for this on the backend, so have just set as "any"')
+                            order = 'any'
+                        response = wc.store_campaign_keyword(
+                            campaign_id=new_campaign_id,
+                            keyword_text=keyword,
+                            keyword_type=kw_set,
+                            keyword_order=order,
+                        )
+                        print('Store positive keyword - response:', response)
+
+                response = wc.start_campaign_feed(new_campaign_id)
+                print('Start campaign - response:', response)
+
+    from TLInterface.get_campaigns import get_all_campaigns
+    all_campaigns = get_all_campaigns()
+
+    from helpers.countries import get_countries
+    countries_list = get_countries()
+
+    return render_template('pages/campaigns/main.html', campaigns=all_campaigns, countries_list=countries_list)
+
+
+@campaigns.route('/<int:campaign_id>', methods=['GET', 'POST'])
+def view_campaign(campaign_id):
 
     from TLInterface.get_campaigns import get_all_campaigns
 
-    campaigns = get_all_campaigns()
+    all_campaigns = get_all_campaigns()
 
-    return render_template('pages/campaigns.html', campaigns=campaigns)
+    if request.method == 'POST':
+        selected_campaign = request.form['campaign-selection']
+        print('SELECTED CAMPAIGN:', selected_campaign)
+        return redirect(url_for('campaigns.view_campaign', campaign_id=selected_campaign))
+
+    return render_template('pages/campaigns/view_campaign.html', all_campaigns=all_campaigns, selected_campaign=campaign_id)
