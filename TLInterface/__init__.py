1c931e40f09dc4992ce17bd26cb0dfb45d5cfbb9
Vince Maina <vchapandrews@gmail.com>
2022-11-07 19:51:49 +0000

    / Changed to get_web_connection class + Added new save_web_connection class for saving the latest version of wc object to flask session

--- TLInterface/__init__.py
+++ TLInterface/__init__.py
@@ -3,11 +3,12 @@ This module serves as an intermediary between my frontend application and Dave's
 """
 
 import logging
-# from TLApi.AccessTLIServer.access_tli import WebConnection, web_conn, authenticate_user
 from TLApiWeb.connect import WebConnection
+from flask import session, has_request_context
+import pickle
 
 
-def authenticate_user(user, password):
+def authenticate_user(user: str, password: str) -> WebConnection:
     wc = WebConnection()
     if wc.url is None:
         wc.url = 'https://tlapi.ictbenchmark.org/'
@@ -17,6 +18,7 @@ def authenticate_user(user, password):
     ret_dict = wc.authenticate_user()
     logging.info(f'{ret_dict}')
     ret_dict = wc.is_authenticated()
+    return wc
 
 
 def web_conn(usr=None, pwd=None, url=None, sessionid=None):
@@ -29,18 +31,58 @@ def web_conn(usr=None, pwd=None, url=None, sessionid=None):
     return wc
 
 
-def get_web_connection(user: str = 'd.hagan', password: str = 'changeme') -> WebConnection or bool:
+# TODO: Change this function so it doesn't use dave's tweet locator login details. This will require me to first add the create client account function that dave added.
+def get_web_connection(username: str = None, password: str = None) -> WebConnection or False:
+
+    """This function creates a new WebConnection object, or fetches it from session object if it already exists."""
 
     try:
-        authenticate_user(user, password)
+        url = 'https://tlapi.ictbenchmark.org/'
+        wc = session.get('wc') if has_request_context() else None
+
+        # if (username is None or password is None) and has_request_context():
+        #     username = session.get('tl_username')
+        #     password = session.get('tl_password')
+
+        # if username is None or password is None:
+        #     print('No user details provided as arguments or in session object.')
+        # else:
+        #     try:
+        #         wc = authenticate_user(username, password)
+        #     except Exception as e:
+        #         logging.error(f'Failed to authenticate user, due to following error:\n\n{e}')
+        #         return False
+
+        if wc:
+            wc = pickle.loads(wc)
+            print('Returning WebConnection saved in session.: ', wc.__dict__)
+
+            if wc.userid and wc.password:
+                response = wc.authenticate_user()
+                logging.info(f'{response}')
+
+        else:
+            wc = WebConnection(userid=None, password=None, url=None, sessionid=None)
+            wc.userid = username
+            wc.password = password
+            wc.set_headers()
+            print('Created new WebConnection: ', wc.__dict__)
+        if wc.url is None:
+            wc.url = url
+        save_web_connection(wc)
+        return wc
     except Exception as e:
-        logging.error(f'Failed to authenticate user, due to following error:\n\n{e}')
+        print(f'Failed to create web connection, due to following error:\n\n{e}')
         return False
 
+
+def save_web_connection(wc):
     try:
-        wc = web_conn()
+        if has_request_context():
+            session['wc'] = pickle.dumps(wc)
+            print('Saved WebConnection to session.')
     except Exception as e:
-        logging.error(f'Failed to create web connection, due to following error:\n\n{e}')
+        print(e)
         return False
-
-    return wc
+    else:
+        return True
