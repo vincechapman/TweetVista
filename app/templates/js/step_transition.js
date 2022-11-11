24e54e2ee9239d028f02e116dc65397e202402bc
Vince Maina <vchapandrews@gmail.com>
2022-11-11 14:08:16 +0000

    / Moved step transition out into its own js file

---
+++ app/templates/js/step_transition.js
@@ -0,0 +1,25 @@
+let signupModal = document.getElementById('signup-modal')
+
+// This script handles the transitioning between each step of the signup sequence
+function stepTransition(elem = undefined, nextStepId = undefined) {
+    signupModal.classList.add('is-active')
+    if (elem != undefined) {
+        while (elem.classList.contains('signup-step') !== true) {
+            elem = elem.parentNode
+        }
+
+        elem.classList.add('hidden')  // Hiding the old step
+
+        if (nextStepId === undefined) {
+            nextStepId = elem.getAttribute('data-next-step')
+        }
+    }
+
+    if (nextStepId === 'none') {
+        document.getElementById('signup-modal').classList.remove('is-active')
+        window.location.href = '{{ url_for('index') }}'
+    }
+
+    let nextStepElem = document.getElementById(nextStepId)
+    nextStepElem.classList.remove('hidden')  // Revealing the next step
+}
