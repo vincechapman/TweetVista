7770b5a35a20b95231f3ef79e94e085550da9d87
Vince Maina <vchapandrews@gmail.com>
2022-12-12 14:50:43 +0000

    + Support for saving tweets to locker via api call

--- app/blueprints/tweet_locker.py
+++ app/blueprints/tweet_locker.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flask_login import login_required
 
 locker = Blueprint('tweet_locker', __name__, url_prefix='/locker')
@@ -11,10 +11,15 @@ def main():
     return render_template('pages/tweet_locker/main.html')
 
 
-@locker.route('/<int:campaign_id>', methods=['GET'])
+@locker.route('/<int:campaign_id>', methods=['GET', 'POST'])
 def view_campaign_locker(campaign_id):
 
     from TLInterface.tweet_locker import get_saved_tweets
     saved_tweets = get_saved_tweets(campaign_id)
 
-    return render_template('pages/tweet_locker/view_locker.html', saved_tweets=saved_tweets)
+    if request.method == 'POST':
+        return jsonify(saved_tweets) if saved_tweets else jsonify(False)
+
+    else:
+        return render_template('pages/tweet_locker/view_locker.html', saved_tweets=saved_tweets)
+
