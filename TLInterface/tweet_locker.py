fd56371945764cb0a221fe93f8cdf4e98067ed15
Vince Maina <vchapandrews@gmail.com>
2022-12-12 14:58:29 +0000

    + Improve code quality

--- TLInterface/tweet_locker.py
+++ TLInterface/tweet_locker.py
@@ -1,15 +1,26 @@
+import logging
 from TLInterface import get_web_connection
 
-wc = get_web_connection()
+wc = get_web_connection(username='test13@gmail.com', password='test13')
 
 
-def get_saved_tweets(campaign_id):
+def get_saved_tweets(campaign_id) -> list[dict] or False:
 
-    response = wc.get_tweet_locker(campaign_id)
+    try:
 
-    return response
+        response = wc.get_tweet_locker(campaign_id)
+
+        if response.get('status') == 200:
+            return response.get('data')
+        else:
+            logging.error(response.get('msg'))
+            return False
+
+    except Exception as e:
+        logging.error(e)
+        return False
 
 
 if __name__ == '__main__':
 
-    pass
+    get_saved_tweets(60)
