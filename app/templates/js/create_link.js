6aee9926090397c308da82d8c068b0dd397e9187
Vince Maina <vchapandrews@gmail.com>
2022-12-28 11:34:13 +0000

    + New function that can create a link and append query parameters to it

---
+++ app/templates/js/create_link.js
@@ -0,0 +1,17 @@
+function create_link(data, base = window.location.href) {
+
+    let urlString = base.split('?')
+    base = urlString[0]
+    let paramsString = urlString[1]
+    let searchParams = new URLSearchParams(paramsString);
+
+    // searchParams.has("name") === true; // true
+    // searchParams.get("age") === "1337"; // true
+
+    for (let [key, value] of Object.entries(data)) {
+        console.log(key, value);
+        searchParams.set(key, String(value))
+    }
+
+    return base + '?' + searchParams.toString()
+}
