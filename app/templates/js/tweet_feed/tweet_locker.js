921acfe0913fe5c1d0e01d42458cf714610d36d4
Vince Maina <vchapandrews@gmail.com>
2022-12-28 20:00:39 +0000

    + Added functionality for deleting tweets from locker / renamed function and route

--- app/templates/js/tweet_feed/tweet_locker.js
+++ app/templates/js/tweet_feed/tweet_locker.js
@@ -1,9 +1,9 @@
-function saveTweet(elem) {
+function saveTweetToLocker(elem) {
 
     let tweetId = elem.getAttribute('data-tweet')
     let campaignId = elem.getAttribute('data-campaign')
 
-    fetch('{{ url_for("api_feed.save_tweet") }}', {
+    fetch('{{ url_for("api_feed.save_tweet_to_locker") }}', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -22,6 +22,36 @@ function saveTweet(elem) {
                 imgElem.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/768px-Flat_tick_icon.svg.png"
                 imgElem.style.height = "18px"
                 imgElem.style.width = "18px"
+                tweetLocker.push(String(tweetId))
+            }
+        })
+}
+
+function deleteTweetFromLocker(elem) {
+
+    let tweetId = elem.getAttribute('data-tweet')
+    let campaignId = elem.getAttribute('data-campaign')
+
+    fetch('{{ url_for("api_feed.delete_tweet_from_locker") }}', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'campaignId': campaignId,
+            'tweetId': tweetId
+        })
+    })
+        .then((response) => response.json())
+        .then((success) => {
+            if (success) {
+                elem.lastElementChild.classList.remove('has-text-success')
+                let imgElem = elem.querySelector('span > img')
+                imgElem.src = "{{ url_for('static', filename='images/icons/Plus-Icon.png') }}"
+                imgElem.style.height = "initial"
+                imgElem.style.width = "initial"
+                tweetLocker.splice(tweetLocker.indexOf(String(tweetId)), 1)
             }
         })
 }
