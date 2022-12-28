bedf92a8f9b05e9621c64c48222dc9c4fad0355d
Vince Maina <vchapandrews@gmail.com>
2022-12-28 20:01:37 +0000

    + Added functionality for adding tweets to excludedTweets

---
+++ app/templates/pages/campaigns/excludeTweets.js
@@ -0,0 +1,18 @@
+function excludeTweets(elem, tweetId) {
+    fetch("{{ url_for('api_campaigns.exclude_tweets') }}", {
+        method: "POST",
+        body: JSON.stringify({
+                'campaignId': campaignId,
+                'tweetId': tweetId
+            })
+    })
+        .then((response) => response.json())
+        .then((success) => {
+            if (success) {
+                console.log("Tweet successfully excluded from showing up again.")
+                elem.closest('.tweet-container').remove()
+            } else {
+                alert("Failed to exclude tweet. Please try again later or contact us.")
+            }
+        })
+}
