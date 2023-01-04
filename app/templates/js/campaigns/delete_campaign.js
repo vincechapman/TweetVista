c074e1b3008b0df7748697841517ee1171537071
Vince Maina <vchapandrews@gmail.com>
2023-01-04 00:13:02 +0000

    + Added alert that prompts users to pause campaigns before trying to delete

--- app/templates/js/campaigns/delete_campaign.js
+++ app/templates/js/campaigns/delete_campaign.js
@@ -8,22 +8,26 @@ function getCampaignDetails(elem) {
     }
 }
 
-function deleteCampaign(elem) {
-    let data = getCampaignDetails(elem)
-    fetch('{{ url_for("api_campaigns.delete_campaign") }}', {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'campaignID': data['id'],
-            'campaignName': data['name'],
+function deleteCampaign(elem, isActive) {
+    if (isActive === "True") {
+        alert('Please pause the campaign before deleting.')
+    } else {
+        let data = getCampaignDetails(elem)
+        fetch('{{ url_for("api_campaigns.delete_campaign") }}', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'campaignID': data['id'],
+                'campaignName': data['name'],
+            })
         })
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            window.location.reload()
-        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                window.location.reload()
+            })
+    }
 }
